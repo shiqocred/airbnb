bdd3f53addf66efe9e4ec4653009e9104dbfa937
shiqocred <21103080090@student.uin-suka.ac.id>
2023-06-05 13:35:46 +0700

    first commit

--- app/components/partials/Map.tsx
+++ app/components/partials/Map.tsx
@@ -8,6 +8,7 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+//@ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconUrl: markerIcon.src,
